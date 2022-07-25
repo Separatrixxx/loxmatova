af6377f49d459f670608e48cc721b66fe3290f90
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-25 14:13:32 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="w-full h-screen bg-blue-500">
 
-    </div>
+    if (document.location.pathname === '/') {
+        document.title = 'LOxmatova';
+    } else {
+        document.title = '404';
+    }
+
+  return (
+      <div className="scroll-smooth bg-neutral-900">
+          <Router>
+              <Routes>
+                  <Route path='/' element={<Home/>} />
+                  <Route path='*' element={<NotFound/>} />
+              </Routes>
+          </Router>
+      </div>
   );
 }
 
