dad6efa77d84ecda97a3b7c0a4f6e38f9858d483
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-26 03:19:26 +0300

    Add files via upload

---
+++ src/components/ContactForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+function ContactForm() {
+    return(
+        <div className="flex mt-7">
+            <div>
+                <div className="flex flex-col contacts_5">
+                    <label htmlFor="formName" className="text-sm lg:text-base text-neutral-800">Имя:</label>
+                    <input id="formName" type="text" name="name" className="w-72 h-8 rounded-lg mt-1 bg-neutral-200 text-neutral-800 px-3 focus:outline-none hover:bg-neutral-300 focus:bg-neutral-300 transition-colors duration-300 ease-in-out" />
+                </div>
+                <div className="flex flex-col mt-3 contacts_6">
+                    <label htmlFor="formEmail" className="text-sm lg:text-base text-neutral-800">E-mail:</label>
+                    <input id="formEmail" type="text" name="email" className="w-72 h-8 rounded-lg mt-1 bg-neutral-200 text-neutral-800 px-3 focus:outline-none hover:bg-neutral-300 focus:bg-neutral-300 transition-colors duration-300 ease-in-out" />
+                </div>
+                <div className="flex flex-col mt-3 contacts_7">
+                    <label htmlFor="formMessage" className="text-sm lg:text-base text-neutral-800">Сообщение:</label>
+                    <textarea name="message" id="myText" className="w-72 h-24 rounded-lg mt-1 bg-neutral-200 text-neutral-800 px-3 py-1 focus:outline-none hover:bg-neutral-300 focus:bg-neutral-300 transition-colors duration-300 ease-in-out"></textarea>
+                </div>
+                <div className="mt-7 contacts_8">
+                    <button id="contacts_btn" className="w-56 h-10 rounded-lg bg-neutral-200 text-violet text-sm lg:text-base hover:bg-violet hover:text-neutral-200 transition-colors duration-300 ease-in-out">Отправить сообщение</button>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default ContactForm;
