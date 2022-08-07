1059d91821843f9b791f6150b9b309408b87326c
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-08-08 00:41:41 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,7 @@ function Home () {
         <div className="scroll-smooth bg-white">
             <Header />
             <div id="first_div" className="w-full h-screen bg-white" onTouchStart={closeMenu}>
-                <input id="qqq" type="text" name="name" className="mt-20 w-72 h-8 rounded-lg mt-1 bg-neutral-200 text-neutral-800 px-3 focus:outline-none hover:bg-neutral-300 focus:bg-neutral-300 transition-colors duration-300 ease-in-out" />
-                <button id="www" className="w-56 h-10 rounded-lg bg-neutral-200 text-violet text-sm lg:text-base hover:bg-violet hover:text-neutral-200 transition-colors duration-300 ease-in-out">Хоба</button>
-                <button id="eee" className="w-56 h-10 rounded-lg bg-neutral-200 text-violet text-sm lg:text-base hover:bg-violet hover:text-neutral-200 transition-colors duration-300 ease-in-out">Опа</button>
+
             </div>
             <Footer />
         </div>
